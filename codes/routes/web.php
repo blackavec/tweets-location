d837db4779da1cf43aee5813f406b4a0cef90962
Mahan <eng.mahan.hazrati@gmail.com>
2016-11-18 17:23:37 +0700

    fix auth

--- codes/routes/web.php
+++ codes/routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
-Route::get('/', 'HomeController@home')->name('home');
+Route::group(['middleware' => 'auth.twitter'], function () {
+    Route::get('/', 'HomeController@home')->name('home');
+});
+
+Route::get('twitter-auth-redirect-url', 'AuthController@twitterAuthRedirectUrl');
